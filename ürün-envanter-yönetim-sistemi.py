99c3de131470a7fce722090a661aa1e0f1ec0c13
Atakan <atakanavseveer@gmail.com>
2025-07-27 00:44:29 +0300

    Update productmanagementsystem.py

--- ürün-envanter-yönetim-sistemi.py
+++ ürün-envanter-yönetim-sistemi.py
@@ -1,73 +1,81 @@
-urunler={}
-def urun_ekle():
-    urun_adi=input("lütfen eklemek istediğiniz ürün ismini giriniz:")
-    urun_fiyatı=input("lütfen eklediğiniz ürünün fiyatını giriniz:")
-    urun_stok=int(input("lütfen ürünün stok adedini giriniz:"))
-    urun_kategori=input("ürün kategorisini giriniz:")
+products = {}
 
-    urunler[urun_adi]={"fiyat":urun_fiyatı,"stok":urun_stok,"kategori":urun_kategori}
+def add_product():
+    product_name = input("Please enter the name of the product you want to add: ")
+    product_price = input("Please enter the price of the product: ")
+    product_stock = int(input("Please enter the stock quantity of the product: "))
+    product_category = input("Please enter the category of the product: ")
 
-    print(f"{urun_adi} adlı ürün eklendi")
+    products[product_name] = {
+        "price": product_price,
+        "stock": product_stock,
+        "category": product_category
+    }
 
-def urun_guncelle():
+    print(f"The product '{product_name}' has been added.")
+
+def update_product():
     while True:
-        urun_adi=input("lütfen güncellemek istediğiniz ürünün adını giriniz:")
-        if urun_adi not in urunler:
-            print("girilen isimde ürün bulunamadı")
+        product_name = input("Please enter the name of the product you want to update: ")
+        if product_name not in products:
+            print("Product not found.")
         else:
             break
-    print("1.Ürün fiyatını güncelle")
-    print("2.Ürün stok durumunu güncelle")
 
-    secim=input("lütfen seçiminizi yapınız:(1-2)")
+    print("1. Update product price")
+    print("2. Update product stock")
 
-    if secim=="1":
-        yeni_fiyat=input("lütfen ismini girdiğiniz ürünün yeni fiyatını giriniz:")
-        urunler[urun_adi]["fiyat"]=yeni_fiyat
-        print(f"{urun_adi} ürününün fiyatı {yeni_fiyat} tl olarak güncellendi")
-    elif secim=="2":
-        yeni_stok=int(input("lütfen ismini girdiğiniz ürünün yeni stok adedini giriniz:"))
-        urunler[urun_adi]["stok"]=yeni_stok
-        print(f"{urun_adi} ürününün stok miktarı {yeni_stok} olarak güncellendi")
+    choice = input("Please select an option (1-2): ")
+
+    if choice == "1":
+        new_price = input("Please enter the new price of the product: ")
+        products[product_name]["price"] = new_price
+        print(f"The price of '{product_name}' has been updated to {new_price} TL.")
+    elif choice == "2":
+        new_stock = int(input("Please enter the new stock quantity of the product: "))
+        products[product_name]["stock"] = new_stock
+        print(f"The stock quantity of '{product_name}' has been updated to {new_stock}.")
     else:
-        print("girdiğiniz isimde bir ürün bulunamadı")
+        print("Invalid selection.")
 
-def urun_sil():
-    urun_adi=input("lütfen silmek istediğiniz ürünün ismini giriniz:")
-    if urun_adi in urunler:
-        del urunler[urun_adi]
-        print(f"{urun_adi} isimli ürün silindi")
+def delete_product():
+    product_name = input("Please enter the name of the product you want to delete: ")
+    if product_name in products:
+        del products[product_name]
+        print(f"The product '{product_name}' has been deleted.")
     else:
-        print("girdiğiniz isimde ürün bulunamadı")
+        print("Product not found.")
 
-def urunleri_listele():
-    if not urunler:
-        print("rafta herhangi bir ürün bulunamadı")
+def list_products():
+    if not products:
+        print("No products found.")
     else:
-        for urun_adi, bilgiler in urunler.items():
-            print(f"Ürün:{urun_adi}")
-            print(f"Fiyat:{bilgiler['fiyat']}")
-            print(f"Ürün stoğu:{bilgiler['stok']}")
-            print(f"Kategori:{bilgiler['kategori']}")
+        for product_name, details in products.items():
+            print(f"Product: {product_name}")
+            print(f"Price: {details['price']}")
+            print(f"Stock: {details['stock']}")
+            print(f"Category: {details['category']}")
+            print("-----------------------")
 
 while True:
-    print("1.Ürün ekleme:")
-    print("2.Ürün güncelleme")
-    print("3.Ürün silme")
-    print("4.Ürün listeleme")
-    print("5.Çıkış")
+    print("1. Add Product")
+    print("2. Update Product")
+    print("3. Delete Product")
+    print("4. List Products")
+    print("5. Exit")
 
-    secim=input("lütfen seçiminizi yapınız:(1-5)")
+    choice = input("Please select an option (1-5): ")
 
-    if secim=="1":
-        urun_ekle()
-    elif secim=="2":
-        urun_guncelle()
-    elif secim=="3":
-        urun_sil()
-    elif secim=="4":
-        urunleri_listele()
-    elif secim=="5":
-        print("Çıkış yapılıyor..")
+    if choice == "1":
+        add_product()
+    elif choice == "2":
+        update_product()
+    elif choice == "3":
+        delete_product()
+    elif choice == "4":
+        list_products()
+    elif choice == "5":
+        print("Exiting...")
+        break
     else:
-        print("Geçersiz seçim")   
+        print("Invalid selection.") 
